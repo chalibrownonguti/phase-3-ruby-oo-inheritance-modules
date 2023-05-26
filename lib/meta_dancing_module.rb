b22dfb6bbd60f01cd23fdee1928777d665fad6b5
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-26 11:51:17 +0300

    updates

---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,14 @@
+module MetaDancing
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+  
+  RSpec.describe MetaDancing do
+    let(:dummy_class) { Class.new { extend MetaDancing } }
+  
+    it 'has a metadata method' do
+      expect(dummy_class.metadata).to eq("This class produces objects that love to dance.")
+    end
+  end
+  
